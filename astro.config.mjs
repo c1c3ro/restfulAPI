62a53e76768744eec942a1eacd4354aeb32b95db
Cícero José <cicerosousa205@gmail.com>
2024-03-22 16:24:43 -0300

    still trying this goddamn cors

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  server: {
+    headers: {
+        "Access-Control-Allow-Origin": "https://nad-edu-git-urgencia-novo-nads-projects-178161a5.vercel.app/"
+    }
+  },
   adapter: vercel()
 });
